ea03b887fa98fd149e234dbc87fe7f4a7b7e11c2
Dominik Braun <braun@sternentstehung.de>
2019-06-11 21:31:19 +0200

    defined Db trait

---
+++ src/db.rs
@@ -0,0 +1,19 @@
+use crate::search;
+
+trait Db {
+    fn connect<'a>(&self, con: &'a str, db: &'a str, auth: Box<Auth>) -> Option<Err>;
+    fn all_tables(&self) -> &[String];
+    fn search<'x>(&self, term: &'x str) -> &[search::Res];
+}
+
+#[derive(Debug, Clone)]
+struct Err {
+    code: u16,
+    msg: String,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Auth<'a> {
+    user: &'a str,
+    pass: &'a str,
+}
