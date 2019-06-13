0675cd203cf21914c1a8e0db2cd9f5e3e008678c
aligator <aligator@suncraft-server.de>
2019-06-13 22:02:35 +0200

    WIP: implement connect

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,77 @@
-use std::result;
+use std::result::Result;
+
+use odbc::*;
+
 use crate::search;
 
 trait Db {
-    fn connect<'a>(&self, con: &'a str, db: &'a str, auth: Box<Auth>) -> result::Result<(), Err>;
+    fn connect<'env>(&mut self, connString: &str) -> Result<(), Err>;
     fn all_tables(&self) -> &[String];
-    fn search<'x>(&self, term: &'x str) -> Vec<search::Res>;
+    fn search(&self, term: &str) -> Vec<search::Res>;
+}
+
+struct Odbc<'env> {
+    environment: Environment<Version3>,
+    connection: Connection<'env>,
+}
+
+impl<'env> Odbc<'env> {
+    fn init(&mut self) -> Result<(), Err> {
+        let envRes = create_environment_v3();
+
+        match envRes {
+            Ok(environment) => self.environment = environment,
+            Err(diagnostics) => {
+                // todo fn to create Err out of diagnostics
+                let error = match diagnostics {
+                    Some(diagnostics) => {
+                        Err {
+                            code: 0,
+                            msg: diagnostics.to_string(),
+                        }
+                    }
+                    None => {
+                        Err {
+                            code: 1,
+                            msg: "odbc environment creation failed".to_string(),
+                        }
+                    }
+                };
+
+                return Result::Err(error);
+            }
+        }
+
+        return Result::Ok(())
+    }
+}
+
+impl<'env> Db for Odbc<'env> {
+    fn connect(&mut self, connString: &str) -> Result<(), Err> {
+        info!("odbc: connecting to {}", connString);
+        let connRes = self.environment.connect_with_connection_string(connString);
+        return match connRes {
+            Ok(connection) => {
+                self.connection = connection;
+                Result::Ok(())
+            },
+            Err(diagnostics) => {
+                // todo fn to create Err out of diagnostics
+                return Result::Err(Err {
+                    code: 0,
+                    msg: diagnostics.to_string(),
+                });
+            }
+        }
+    }
+
+    fn all_tables(&self) -> &[String] {
+        unimplemented!()
+    }
+
+    fn search(&self, term: &str) -> Vec<search::Res> {
+        unimplemented!()
+    }
 }
 
 #[derive(Debug, Clone)]
