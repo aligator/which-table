c6e289d3404cb7ac1bf73423a17290d0cfad8757
aligator <aligator@suncraft-server.de>
2019-06-17 17:51:11 +0200

    fix rebase

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#[macro_use]
+extern crate log;
+
 use crate::db::Db;
 use crate::db::Odbc;
 
 mod db;
 mod search;
 
-const CON_STR: &str = "Driver={MySQL ODBC 8.0 ANSI Driver};server=localhost;database=foodunit2;user=foodunit2;password=foodunit2";
-//const CON_STR: &str = "Driver=MariaDB;server=localhost;database=rust;DB=rust;user=rust;password=rust;";
+//const CON_STR: &str = "Driver={MySQL ODBC 8.0 ANSI Driver};server=localhost;database=foodunit2;user=foodunit2;password=foodunit2";
+const CON_STR: &str = "Driver=MariaDB;server=localhost;database=rust;DB=rust;user=rust;password=rust;";
 //const CON_STR: &str = "Driver=SQLite3;Database=test.sqlite3";
 
 fn main() {
