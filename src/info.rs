1a1c98ada4be8b7c5ce8b5a987bbba13b37a59ee
Dominik Braun <braun@sternentstehung.de>
2019-06-17 19:41:44 +0200

    implemented get(), removed DBMS name

--- src/info.rs
+++ src/info.rs
@@ -1,20 +1,22 @@
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Dbms {
-    name: &'static str,
     odbc_table_pos: u8,
 }
 
 impl Dbms {
     pub fn get(name: &str) -> Option<Dbms> {
         let systems = Self::load_all();
-        None
+        
+        return match systems.get(name) {
+            Some(sys) => Some(sys.clone()),
+            None => None,
+        };
     }
 
-    fn new(name: &'static str, odbc_table_pos: u8) -> Dbms {
+    fn new(odbc_table_pos: u8) -> Dbms {
         Dbms {
-            name,
             odbc_table_pos,
         }
     }
@@ -22,10 +24,10 @@ impl Dbms {
     fn load_all() -> HashMap<&'static str, Dbms> {
         let mut sys_map = HashMap::new();
 
-        sys_map.insert("mariadb", Dbms::new("", 0));
-        sys_map.insert("mysql", Dbms::new("", 0));
-        sys_map.insert("mssql", Dbms::new("", 0));
-        sys_map.insert("sqlite", Dbms::new("", 0));
+        sys_map.insert("mariadb", Dbms::new(0));
+        sys_map.insert("mysql", Dbms::new(3));
+        sys_map.insert("mssql", Dbms::new(0));
+        sys_map.insert("sqlite", Dbms::new(0));
 
         sys_map
     }
