02fb82258ff7be848cfaf0319efc2ff63b4496c1
Dominik Braun <braun@sternentstehung.de>
2019-06-17 21:05:18 +0200

    added TableMeta struct

--- src/info.rs
+++ src/info.rs
@@ -2,7 +2,16 @@ use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Dbms {
+    driver_name: &'static str,
     odbc_table_pos: u8,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct TableMeta<'a> {
+    catalogue: Option<&'a str>,
+    schema: Option<&'a str>,
+    table: Option<&'a str>,
+    t_type: Option<&'a str>,
 }
 
 impl Dbms {
@@ -15,19 +24,19 @@ impl Dbms {
         };
     }
 
-    fn new(odbc_table_pos: u8) -> Dbms {
+    fn new(driver_name: &'static str, odbc_table_pos: u8) -> Dbms {
         Dbms {
+            driver_name,
             odbc_table_pos,
-        }
+        } 
     }
 
     fn load_all() -> HashMap<&'static str, Dbms> {
         let mut sys_map = HashMap::new();
 
-        sys_map.insert("mariadb", Dbms::new(0));
-        sys_map.insert("mysql", Dbms::new(3));
-        sys_map.insert("mssql", Dbms::new(0));
-        sys_map.insert("sqlite", Dbms::new(0));
+        sys_map.insert("mariadb", Dbms::new("MariaDB", 2));
+        sys_map.insert("mysql", Dbms::new("MySQL ODBC 8.0 ANSI Driver", 2));
+        sys_map.insert("sqlite", Dbms::new("SQLite3", 1));
 
         sys_map
     }
